8c64a3e4b30bfc86218143d3a7c0fdd9b11597d9
Jennifer Qiao <qiaojennifer97@gmail.com>
2022-07-14 12:08:07 +0300

    generate random circles that do not overlap

--- sketch/circlePacking.ts
+++ sketch/circlePacking.ts
@@ -7,14 +7,42 @@ interface Position {
     y: number;
 }
 
+
 function calculatePackedCircles(areaWidth: number, areaHeight: number): Circle[] {
     //TODO: you need to implement this function properly!  
     //These are just a couple of random circles, with no consideration yet for overlap.
-    return [
-        { pos: { x: 300, y: 300 }, radius: 100 },
-        { pos: { x: random(0, areaWidth), y: random(0, areaHeight) }, radius: 40 },
-    ];
+    
+    const validatedCircles: Circle[]= [];
+
+    for (let i=0; i<1000; i++) {
+        const candidate: Circle = makeCircle(areaWidth,areaHeight);
+        if (!overlap(candidate, validatedCircles)) {
+            validatedCircles.push(candidate)
+        }
+
+    }
+
+    return validatedCircles
 }
+
+function makeCircle(areaWidth: number, areaHeight: number): Circle {
+    const randomCircle: Circle = { pos: { x: random(0, areaWidth), y: random(0, areaHeight) }, radius: random(0,100) }
+    return randomCircle
+
+}
+
+function overlap(candidate: Circle, validatedCircles: Circle[]): boolean {
+  
+    for (const individualCircle of validatedCircles) {
+        if (candidate.radius + individualCircle.radius > distance(candidate.pos, individualCircle.pos)) {
+            return true
+         }
+    }
+
+    return false 
+
+}
+
 
 /** Returns the distance between two given positions.
     This function doesn't require p5.js 
